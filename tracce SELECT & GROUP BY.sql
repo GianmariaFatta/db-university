41920f358643be0d813a0d7b3981da06029993d0
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-02-15 09:50:01 +0100

    fixed track 3 after lesson

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -13,7 +13,7 @@ WHERE`cfu`>10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT `name`,`surname` ,`date_of_birth` 
 FROM `students` 
-WHERE DATE(`date_of_birth`)<='1993-02-14';
+WHERE `date_of_birth` <= DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT `name` , `period`, `year` 
