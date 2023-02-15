eb9525ae23e767b4ce898bd48fc06a0b52888033
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-02-15 17:22:07 +0100

    tracks 1,2,3 completed

--- tracce 15-02.sql
+++ tracce 15-02.sql
@@ -1,13 +1,25 @@
 --1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
-
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`  
+FROM `students`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di laurea in economia';
 
 --2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-
-
+SELECT `degrees`.`name` AS 'Corso', `departments`.`name` AS 'Dipartimento'
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name`= 'Dipartimento di Neuroscienze';
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+SELECT `courses`.`name` AS 'Corso', `teachers`.`name`,`teachers`.`surname`
+FROM `courses`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id`= `teachers`.`id`
+WHERE `teachers`.`id` ='44';
 
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
